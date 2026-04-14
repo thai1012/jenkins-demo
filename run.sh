a77f8adf967fafc2a637f9f0a440e24b7f8fbb85
thai1012 <105804705+thai1012@users.noreply.github.com>
2026-04-14 10:16:06 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "=== Jenkins demo started ==="
+echo "Current directory:"
+pwd
+echo "Files in workspace:"
+ls -la
+echo "Show index.html:"
+cat index.html
+echo "=== Demo success ==="
